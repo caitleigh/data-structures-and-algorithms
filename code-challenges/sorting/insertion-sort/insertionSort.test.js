34e65836febac82338e1cf6653171dab620bc8bc
caitleigh <caitleigh27@gmail.com>
2020-03-16 16:34:49 -0700

    started code challenge 23

---
+++ code-challenges/sorting/insertion-sort/insertionSort.test.js
@@ -0,0 +1,9 @@
+require('./insertionSort')
+
+describe('Insertion Sort', () => {
+
+    it('Succesfully returns a sorted array', () => {
+        arr = [8, 4, 23, 42, 16, 15]
+        expect(insertionSort(arr).toEqual([4, 8, 15, 16, 23, 42]))
+    })
+})
